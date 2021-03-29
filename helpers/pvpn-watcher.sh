a406620b02c4ecf13c2ba4cb54b0f5e0eeb51d40
Yuqian <yuqian.yang@msn.com>
2021-03-29 20:11:21 +0800

    update watcher

--- helpers/pvpn-watcher.sh
+++ helpers/pvpn-watcher.sh
@@ -3,10 +3,16 @@ source /etc/environment
 pvpn=protonvpn
 log_file=/var/log/pvpn-watcher.log
 /bin/date >> $log_file
-($pvpn s 2>%1) >> $log_file
+($pvpn s 2>&1) >> $log_file
 if $pvpn s | grep -q Disconnected; then
-  echo "Trying to reconnect..." >> $log_file
-  ($pvpn c -f 2>%1) >> $log_file
-  echo "Done" >> $log_file
+  echo "[Watcher] Disconnected, trying to reconnect..." >> $log_file
+  ($pvpn d)
+  ($pvpn c -f 2>&1) >> $log_file
+  echo "[Watcher] Done" >> $log_file
+elif $pvpn s | grep -q error; then
+  echo "[Watcher] Error detected, trying to reconnect..." >> $log_file
+  ($pvpn d)
+  ($pvpn c -f 2>&1) >> $log_file
+  echo "[Watcher] Done" >> $log_file
 fi
 echo "------------------------------" >> $log_file
